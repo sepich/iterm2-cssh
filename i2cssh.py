59d17e4c579f0b01c85a289e19c1164f1e0fbadd
sepa <i@sepa.spb.ru>
2019-06-29 11:33:09 +0300

    no close session on end

--- i2cssh.py
+++ i2cssh.py
@@ -4,11 +4,9 @@ import sys
 from math import sqrt, ceil
 
 profile = 'Default'
-#shell='/usr/bin/env bash -l'  # if set, each session would also have shell before ssh
 
 
 async def main(connection):
-    # init
     if len(sys.argv) == 1:
         print('No hosts to connect to found! Usage is:\ni2cssh.py host1 [host2 ...]')
         return
@@ -22,26 +20,24 @@ async def main(connection):
     sessions = [None for x in range(cols)]  # sessions to split down
     col = 0
     for host in hosts:
-        if col == cols:
-            col = 1
-        else:
-            col += 1
+        tmp_profile = iterm2.LocalWriteOnlyProfile()
+        tmp_profile.set_use_custom_command('Yes')
+        tmp_profile.set_command(f'ssh {host}')
+        tmp_profile.set_close_sessions_on_end(False)
+
         if not tab:  # create new tab first
-            tab = await window.async_create_tab(profile=profile, command=f'ssh {host}')
-            sessions[col-1] = tab.current_session
+            tab = await window.async_create_tab(profile=profile, profile_customizations=tmp_profile)
+            sessions[col] = tab.current_session
         else:
-            tmp_profile = iterm2.LocalWriteOnlyProfile()
-            tmp_profile.set_use_custom_command('Yes')
-            tmp_profile.set_command(f'ssh {host}')
-            if sessions[col-1]:  # split down
-                sessions[col-1] = await sessions[col-1].async_split_pane(vertical=False, profile=profile, profile_customizations=tmp_profile)
+            if sessions[col]:  # split down
+                sessions[col] = await sessions[col].async_split_pane(vertical=False, profile=profile, profile_customizations=tmp_profile)
             else:  # first row, split right
-                try:
-                    sessions[col-1] = await sessions[col-2].async_split_pane(vertical=True, profile=profile, profile_customizations=tmp_profile)
-                except:
-                    await iterm2.MainMenu.async_select_menu_item(connection, "Split Vertically with Current Profile")
-                    sessions[col-1] = tab.current_session
-                    await tab.current_session.async_send_text(f'ssh {host}\n', suppress_broadcast=True)
+                # https://gitlab.com/gnachman/iterm2/issues/7903
+                if not sessions[col-1]:
+                    return
+                sessions[col] = await sessions[col-1].async_split_pane(vertical=True, profile=profile, profile_customizations=tmp_profile)
+        col = (col+1) % cols
+
 
     # enable broadcast
     for session in tab.sessions:
